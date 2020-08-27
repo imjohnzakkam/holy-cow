329ab7fe78e40f6807cb09a2dba01686be776e25
imjohnzakkam <johnzakkam2592@gmail.com>
2020-08-27 16:56:29 +0530

    upd

--- SegTree/maximalsum.cpp
+++ SegTree/maximalsum.cpp
@@ -61,7 +61,7 @@ vl getFactorization(ll x)
 
 struct item {
     ll seg, pref, suf, sum;
-}
+};
 
 struct segtree {
     int size;
@@ -100,20 +100,66 @@ struct segtree {
             }
             return;
         }
-        int m = (lx+rx)/2;
-        build(a, 2*x+1, lx, m);
-        build(a, 2*x+2, m, rx);
-        values[x] = merge(values[2*x+1], values[2*x+2]);
+        int m = (lx + rx)/2;
+        build(a, 2 * x + 1, lx, m);
+        build(a, 2 * x + 2, m, rx);
+        values[x] = merge(values[2 * x + 1], values[2 * x + 2]);
     }
 
-    
+    void build(vl &a) {
+        build(a, 0, 0, size);
+    }
+
+    void set(int i, int v, int x, int lx, int rx) {
+        if(rx-lx==1) {
+            values[x] = single(v);
+            return;
+        }
+        int m = (lx + rx)/2;
+        if(i < m) {
+            set(i, v, 2 * x + 1, lx, m);
+        }
+        else {
+            set(i, v, 2 * x+2, m ,rx);
+        }
+        values[x] = merge(vlues[2 * x + 1], values[2 * x + 2]);
+    }
+
+    void set (int i, int v) {
+        set(i, v, 0, 0, size);
+    }
+
+    item calc (int l, int r, int x, int lx, int rx) {
+        if(lx >= r or lx >= rx) return NEUTRAL_ELEMENT;
+        if(lx >= l and rx <= r) return values[x];
+        int m = (lx + rx) / 2;
+        item s1 = calc(l, r, 2 * x + 1, lx, m);
+        item s2 = calc(l, r, 2 * x + 2, m , rx);
+        return merge(s1, s2);
+    }
+
+    item calc (item l, item r) {
+        return calc(l, r, 0, 0, size);
+    }
 
 };
 
 void check()
 {
+    ll n,m;
+    cin >> n >> m;
     segtree st;
-
+    st.init(n);
+    vl a(n);
+    for(int i = 0; i < n; i++) cin >> a[i];
+    st.build(a);
+    cout << st.calc(0,n).seg << endl;
+    while(m--) {
+        int i, v;
+        cin >> i >> v;
+        st.set(i, v);
+        cout << st.calc(0, n).seg << endl;
+    }
     return ;
 }
 
@@ -121,7 +167,7 @@ int32_t main()
 {
     fastio;
     ll t = 1;
-    cin >> t;
+    // cin >> t;
     while(t--)
         check();
     return 0;
